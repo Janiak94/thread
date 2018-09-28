002d21fc58a390a65efe5f636fbeb11b0fbcd19b
Filip <filhei@student.chalmers.se>
2018-09-28 15:46:40 +0200

    Crude working program everything is best eu

--- src/main.c
+++ src/main.c
@@ -1,23 +1,83 @@
 #include <stdio.h>
 #include <complex.h>
 #include <math.h>
+#include <stdlib.h>
 
-// d is the degree of the polynomial, return exact roots
-void find_roots_exact(int d, double *roots_exact){
-	roots_exact[0]=1;
+#define TOL 1e-6
+#define MAX_VAL 1e10
+// Global variables
+double complex * roots_exact;
+int d;
+
+// f(x) = x^d-1
+double complex iterate(double complex x) {
+	
+	double complex value = x;
+	for (int i = 1; i<d-1; ++i) {
+		value *= x;
+	//	printf("Inside f.   Value = %f + %f\n", creal(value), cimag(value));
+	}
+	return x - (value*x - 1)/ (d*value);;
+}
+
+
+int close_to_root(double complex x) {
+	printf("Test 2\n");
+	if (creal(x) > MAX_VAL || creal(x) < -MAX_VAL || cimag(x) > MAX_VAL 
+			|| cimag(x) < - MAX_VAL)
+		return d;
+
+	for (int i = 0; i<d; ++i) {
+		if (  creal((x-roots_exact[i])*conj(x-roots_exact[i])) <= TOL )
+			return i;
+	}
+	printf("Test 3 \n");
+	return -1;
+}
+
+
+int find_root(double complex x_k) {
+	// TODO: Check condition every other iteration?
+	int root_idx; 
+	int i = 0;
+	printf("Test 1\n");
+	while ( (root_idx = close_to_root(x_k) ) == -1  ) {
+		x_k = iterate(x_k);
+		printf("x_%d = %f + %f i, \n", i, creal(x_k), cimag(x_k));
+		++i;
+	}	
+	return root_idx;
 }
 
 int main(void){
 	
-	int d;
-	d = 5;
+	int debug = 1;
+	d = 3;
+	// =========================
 	// DEFINE CORRECT ROOTS
-	double complex roots_exact[d];
+	// =========================
+	roots_exact = (double complex *) malloc(sizeof(double complex) * d);
 	for (int i = 0; i<d; ++i) {
 		roots_exact[i] = cos(2*M_PI*i/d) + sin(2*M_PI*i/d) * I;
 	}
-	for (int i = 0; i<d; i++) {
-		printf("Root %d = %f + %f i\n", i, creal(roots_exact[i]), cimag(roots_exact[i]));
+	// print roots for debugging
+	if (debug) {
+		for (int i = 0; i<d; i++) {
+			printf("Root %d = %f + %f i\n", i, creal(roots_exact[i]), cimag(roots_exact[i]));
+		}
 	}
+
+	// Test iterate function
+	double complex test_number = -3 + 1*I;
+	double complex test_answer = find_root(test_number);
+	if (debug)
+		printf("Test function_f. x = %f + %f i, f(x) = %f + %f i\n", 
+			creal(test_number), cimag(test_number),
+			creal(test_answer), cimag(test_answer));
+
+
+	
+	
+
 
 }
