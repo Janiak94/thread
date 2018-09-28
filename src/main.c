429f2d17d36de4cbc9e8d042c29db0bb7a9b5185
Filip <filhei@student.chalmers.se>
2018-09-28 14:23:10 +0200

    Added functionality to calculate correct roots.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-//hej /Tobias
+#include <complex.h>
+#include <math.h>
 
 // d is the degree of the polynomial, return exact roots
 void find_roots_exact(int d, double *roots_exact){
@@ -8,9 +8,16 @@ void find_roots_exact(int d, double *roots_exact){
 }
 
 int main(void){
-	double roots_exact[1];
-	find_roots_exact(1, roots_exact);
-	printf("%d\n", roots_exact[0]);
-	return 0;
-	// test - Filip was here
+	
+	int d;
+	d = 5;
+	// DEFINE CORRECT ROOTS
+	double complex roots_exact[d];
+	for (int i = 0; i<d; ++i) {
+		roots_exact[i] = cos(2*M_PI*i/d) + sin(2*M_PI*i/d) * I;
+	}
+	for (int i = 0; i<d; i++) {
+		printf("Root %d = %f + %f i\n", i, creal(roots_exact[i]), cimag(roots_exact[i]));
+	}
+
 }
